f0cc158ebb8377e5b0b0d5e35dcf182dbfac4b3d
unknown <afif@imperialhealthtech.com>
2022-02-02 22:44:02 +0700

    api jasper aziz

---
+++ app/Models/Jpp.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jpp extends Model
+{
+    use HasFactory;
+    protected $table ="jpps";
+    protected $primaryKey='id';
+    protected $fillable = [
+        'kodeCounter',
+        'jenisJasper',
+        'latitude',
+        'longtitude',
+        'penanggungJawab'  
+    ];
+}
