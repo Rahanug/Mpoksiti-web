11e2b09491d60badf3872100af77f5620dcb97a6
Azizy-Github <afif.azizy@gmail.com>
2022-03-19 20:07:13 +0700

    update image model to include location

--- app/Http/Controllers/JPPController.php
+++ app/Http/Controllers/JPPController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Jpp;
 use App\Models\Ppk;
+use App\Models\Trader;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Carbon;
@@ -18,11 +19,23 @@ class JPPController extends Controller
     }
 
     public function pemeriksaan() {
-        $ppkModel = new Ppk();
+        $ppkModel = new Ppk();  
+        $traderModel = new Trader();
         $list_ppk = $ppkModel->where("kode_counter_jpp", Auth::user()->kode_counter)->get();
+        $list_data_ppk = array();
+        $count = 0;
+        foreach ($list_ppk as $ppk) {
+            $trader = $traderModel->where('id_trader', $ppk->id_trader)->get();
+            array_push($list_data_ppk, [
+                'nama_trader' => $trader[0]->nm_trader,
+                'tgl' => 'tes'
+            ]);
+            $count++;
+        }
         return view('jpp.pemeriksaan', [
             "title" => "pemeriksaan",
-            "list_ppk" => $list_ppk
+            "list_ppk" => $list_ppk,
+            "list_data_ppk" => $list_data_ppk
         ]);
     }
     
--- app/Http/Controllers/api/ImageController.php
+++ app/Http/Controllers/api/ImageController.php
@@ -16,9 +16,13 @@ class ImageController extends Controller
     public function store(Request $request)
     {
         
-        $image = new Image;        
+        $image = new Image;
+        $image->id_ppk = (int)$request->id_ppk;
         $image->no_aju_ppk = $request->no_aju_ppk;
         $image->id_trader = (int)($request->id_trader);
+        $image->latitude = $request->latitude;
+        $image->longitude = $request->longitude;
+        $image->kd_ikan = $request->kd_ikan;
         if ($request->hasFile('image')) {
             $path = $request->file('image')->store('pemeriksaan_klinis');            
             $image->url_file = $path;
